5e22447c0105149533f9b52d98a9bacacdc5f21d
DanielTran24 <dtran0113@gmail.com>
2024-11-29 20:24:32 -0600

    port forwarding

--- KV.py
+++ KV.py
@@ -1,10 +1,14 @@
 import logging
-from flask import Flask
+from flask import Flask, request, jsonify
 import threading
 from datetime import datetime
 import json
 import os
 from HashRing import HashRing
+import socket
+import requests
+import hashlib
+
 
 """
 Command Line
@@ -29,8 +33,13 @@ kv_store_lock = threading.Lock() # Locking mechanism to handle concurrent reques
 KV_LOG_FILE = 'kv_store_log.txt' # File to store data as backup in case of app failure
 
 hash_ring = HashRing(num_virtual_nodes=100)
-for store_id in range(num_stores):
-    hash_ring.add_node(store_id)
+hash_ring.add_node("127.0.0.1:5980")
+hash_ring.add_node("127.0.0.1:5981")
+hash_ring.add_node("127.0.0.1:5982")
+# for store_id in range(num_stores): 
+#     # Currently our store ids are 0,1,2
+#     # hash_ring.add_node(store_id + 5980)
+#     hash_ring.add_node(store_id)
 
 def persist_to_file():
     with open(KV_LOG_FILE, 'w') as f:
@@ -58,12 +67,26 @@ def get_value(key):
     if not key:
         return f"ERROR: must enter a nonempty key.\n Time: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n", 400
 
-    store_id = hash_ring.get_store(key)
+    store_id = hash_ring.get_store(key) # --> "127.0.0.1:{port_number}"
+    local_store_id = f"127.0.0.1:{app.config['PORT']}"
+
+    if store_id != local_store_id:
+        try:
+            response = requests.get(f"http://{store_id}/get/{key}")
+            return response.content, response.status_code
+        except requests.exceptions.RequestException as e:
+            return f"ERROR: Unable to reach server {store_id}.\nDetails: {e}\n", 500
+
+    dictionary_index = int(hashlib.sha256(key.encode()).hexdigest(), 16) % 3
+
     with kv_store_lock:
-        if key in kv_stores[store_id]:
-            return f"Value: {kv_stores[store_id][key]}.\n Time: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n", 200
+        if key in kv_stores[dictionary_index]:
+            value = kv_stores[dictionary_index][key]
+            return f"Value: {value}\nTime: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n", 200
+            # return ""
         else:
             return f"Key not Found.\n {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n", 404
+            # return ""
 
 @app.route('/post/<key>/<value>', methods=['POST'])
 def add_value(key, value):
@@ -71,28 +94,48 @@ def add_value(key, value):
         return f"ERROR: Key and value cannot be empty.\n Time: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n", 400
 
     store_id = hash_ring.get_store(key)
-    with kv_store_lock:
-        if key in kv_stores[store_id]:
-            return f"ERROR: Key already exists.\n Time: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n", 409
-        kv_stores[store_id][key] = value
-        persist_to_file()
-    return f"Successfully created KV pair: {key} -> {kv_stores[store_id][key]}.\n Time: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n", 201
+    local_store_id = f"127.0.0.1:{app.config['PORT']}"
+    if store_id != local_store_id:
+        try:
+            response = requests.post(f"http://{store_id}/post/{key}/{value}")
+            return response.content, response.status_code
+        except requests.exceptions.RequestException as e:
+            return f"ERROR: Unable to reach server {store_id}.\nDetails: {e}\n", 500
 
-@app.route('/update/<key>/<value>', methods=['PUT'])
+    dictionary_index = int(hashlib.sha256(key.encode()).hexdigest(), 16) % 3
+
+    with kv_store_lock:
+        if key in kv_stores[dictionary_index]:
+            return f"ERROR: Key already exists.\n Time: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n", 409
+        kv_stores[dictionary_index][key] = value
+        persist_to_file()
+    return "" # Key added successfully
+    
+@app.route('/put/<key>/<value>', methods=['PUT'])
 def update_value(key, value):
     if not key or not value:
         return f"ERROR: key and value can't be empty.\n Time: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n"
     
     store_id = hash_ring.get_store(key)
+    local_store_id = f"127.0.0.1:{app.config['PORT']}"
+
+    if store_id != local_store_id:
+        try:
+            response = requests.put(f"http://{store_id}/put/{key}/{value}")
+            return response.content, response.status_code
+        except requests.exceptions.RequestException as e:
+            return f"ERROR: Unable to reach server {store_id}.\nDetails: {e}\n", 500
+    
+    dictionary_index = int(hashlib.sha256(key.encode()).hexdigest(), 16) % 3
+
     with kv_store_lock:
-        if key not in kv_stores[store_id]:
+        if key not in kv_stores[dictionary_index]:
             return f"ERROR: Key: '{key}' not found.\n Time: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n", 404
-        
-        kv_stores[store_id][key] = value
+        kv_stores[dictionary_index][key] = value
         persist_to_file()
 
     #app.logger.info statement to verify that the value was updated correctly
-    return f"KV pair updated: {key} -> {kv_stores[store_id][key]}.\n Time: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n", 200
+    return ""
 
 @app.route('/delete/<key>', methods=['DELETE'])
 def delete_value(key):
@@ -100,11 +143,22 @@ def delete_value(key):
         return f"ERROR: must enter a nonempty key.\n Time: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n", 400
 
     store_id = hash_ring.get_store(key)
+    local_store_id = f"127.0.0.1:{app.config['PORT']}"
+    
+    if store_id != local_store_id:
+        try:
+            response = requests.delete(f"http://{store_id}/delete/{key}")
+            return response.content, response.status_code
+        except requests.exceptions.RequestException as e:
+            return f"ERROR: Unable to reach server {store_id}.\nDetails: {e}\n", 500
+
+    dictionary_index = int(hashlib.sha256(key.encode()).hexdigest(), 16) % 3
+    
     with kv_store_lock:
-        if key in kv_stores[store_id]:
-            del kv_stores[store_id][key]
+        if key in kv_stores[dictionary_index]:
+            del kv_stores[dictionary_index][key]
             persist_to_file()
-            return f"Successfully deleted key: {key}.\n Time: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n", 200
+            return ""
         else:
             return f"ERROR: Input key not in store.\n Time: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n", 404
     return 
@@ -112,6 +166,19 @@ def delete_value(key):
 @app.route('/getall', methods=['GET'])
 def get_all():
     data = []
+    parent_port = f"127.0.0.1:5980"
+    if f"127.0.0.1:{app.config['PORT']}" == parent_port:
+        for node in hash_ring.nodes:
+            if node != parent_port:
+                try:
+                    response = requests.get(f"http://{node}/getall")
+                    if response.status_code == 200:
+                        data.append(f"{node}:\n{response.text}")
+                    else:
+                        data.append(f"Error retrieving data from {node}: {response.status_code}")
+                except requests.exceptions.RequestException as e:
+                    return f"ERROR: Could not retrieve data from {node}\n Details: {e}"
+            
     with kv_store_lock:
         for i, store in enumerate(kv_stores):
             store_data = ', '.join([f"{key}={value}" for key, value in store.items()])
@@ -120,12 +187,59 @@ def get_all():
 
 @app.route('/deleteall', methods=['DELETE'])
 def delete_all():
+    errors = []
+    # local_store_id = f"127.0.0.1:{app.config['PORT']}"
+    
+    # if local_store_id in hash_ring.nodes:
+    #     hash_ring.nodes.remove(local_store_id) # Make sure we remove it from the list of nodes to call deleteall on
+
+    parent_port = f"127.0.0.1:5980"
+    if f"127.0.0.1:{app.config['PORT']}" == parent_port:
+        for node in hash_ring.nodes:
+            if node != parent_port:
+                try:
+                    response = requests.delete(f"http://{node}/deleteall") # Node = address??
+                    if response.status_code != 200:
+                        errors.append(f"Node {node}: {response.text}")
+                except requests.RequestException as e:
+                    errors.append(f"Node {node}: {str(e)}")
+            
     with kv_store_lock:
         for store in kv_stores:
             store.clear()
         persist_to_file()
+            
+    if errors:
+        return jsonify({"error": "Failed to del from som store", "details": errors}), 500
+    
     return f"Successfully deleted all key-values:\n Time: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n", 200
 
+# @app.route('/<operation>/<key>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+# def forward_request(operation, key):
+#     # forward req to the right store
+#     target_port = hash_ring.get_node(key)
+#     target_url = f"http://127.0.0.1:{target_port}/{operation}/{key}/{value}"
+    
+#     method = request.method
+#     try:
+#         if method == 'GET':
+#             response = requests.get(target_url)
+#         elif method == 'POST':
+#             value = request.args.get('value')
+#             response = requests.post(target_url, params={'value': value})
+#         elif method == 'PUT':
+#             value = request.args.get('value')
+#             response = requests.dele
+#         elif method == 'DELETE':
+#             value = request.args.get('value')
+#         else:
+        
 if __name__ == '__main__':
+    import sys
     load_from_file()
-    app.run("0.0.0.0", port=5980, debug=True)
+    port = int(sys.argv[1])
+    app.config['PORT'] = port
+    app.run("0.0.0.0", port=port, debug=True)
+    # python3 KV.py 5980
+    # python3 KV.py 5981
+    # python3 KV.py 5982    
